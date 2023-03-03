510b85210fe0ec4e5cea0ff05d52d7eb89bab5bf
CopsGit <github@copsking.com>
2023-03-02 20:18:18 -0500

    REFACTOR: clean logs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -53,8 +53,6 @@ export default function Home() {
                 },
             });
 
-            console.log(response)
-
             if (response.status === 200) {
                 localStorage.setItem('apiKey', apiKeyInput);
                 setApiKey(apiKeyInput);
